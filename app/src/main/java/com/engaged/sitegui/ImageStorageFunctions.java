41d9479bbd1daa82b550c34ce227724ccff79bf0
dadecker <daviddecker21@yahoo.com>
2020-07-18 22:20:17 -0400

    import java.io.File;

--- app/src/main/java/com/engaged/sitegui/ImageStorageFunctions.java
+++ app/src/main/java/com/engaged/sitegui/ImageStorageFunctions.java
@@ -1,6 +1,9 @@
 package com.engaged.sitegui;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
@@ -11,6 +14,8 @@ import android.widget.VideoView;
 
 import java.io.File;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import static android.app.Activity.RESULT_OK;
 import static androidx.core.app.ActivityCompat.startActivityForResult;
@@ -19,27 +24,47 @@ public class ImageStorageFunctions
 {
 
     public static final int UPLOAD_REQUEST = 20;
-    private Button upload_button;
-    private Button download_button;
-    private VideoView stream_video;
     public static final int DOWNLOAD_REQUEST = 30;
     public static final int VIDEO_GALLERY_REQUEST = 40;
     String global_path, global_uid, unique_name;
-    Uri videoUri, specific_video_uri;
-    String specific_video_url;
+    Uri imageURI, specific_image_uri;
+    String specific_image_url;
     public void openGallery() {
         //Intent creates a specific change to a different activity.
-        Intent videoGalleryIntent = new Intent(Intent.ACTION_PICK);
+        Intent storeImageIntent = new Intent(Intent.ACTION_PICK);
 
-        File videoDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
+
 
         Uri temp = null;
 
-        videoGalleryIntent.setDataAndType(temp, "video/*");
+        storeImageIntent.setDataAndType(temp, "image/*");
 
-        Log.d("GalleryActivity" , "******BEFORE storageRef is initialized in openGallery*******");
+    }
 
-        //recieve something back from this result
+    private String saveToInternalStorage(Bitmap bitmapImage, Context context){
+        ContextWrapper cw = new ContextWrapper(context);
+        // path to /data/data/yourapp/app_data/imageDir
+        File imageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        // Create imageDir
+
+
+        File mypath=new File(imageDirectory,"profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return imageDirectory.getAbsolutePath();
     }
 
 
--- app/src/main/java/com/engaged/sitegui/MainActivity.java
+++ app/src/main/java/com/engaged/sitegui/MainActivity.java
@@ -1,45 +1,66 @@
 package com.engaged.sitegui;
 
 
-import com.bumptech.glide.Glide;
-import com.engaged.sitegui.ImageAdapter;
-
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-
-import static com.engaged.sitegui.HandleHttp.postNumber;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private static String numberInput = "";
-    ImageView imageView;
+    Context context;
+    Activity activity;
+    CoordinatorLayout coordinatorLayout;
+    Button downloadbutton;
+    ImageView imageViewLogo;
+    ImageView imageViewLeftSide;
+    ProgressDialog mProgressDialog;
+    URL url;
+    AsyncTask mMyTask;
+    AsyncTask mTaskTwo;
+    Bitmap resizedBM;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ImageView imageView = findViewById(R.id.logoImage);
         setContentView(R.layout.activity_main);
-
-        //ImageView backgroundImage = null;
-        try{
-            //ImageAdapter.ResizeImage(backgroundImage, "arn:aws:s3:us-east-2:864435084730:accesspoint/get-images/background/cafe2.jpg");
-            ImageAdapter.ResizeImage(imageView, "arn:aws:s3:us-east-2:864435084730:accesspoint/get-images/logo/engaged_logo_green.png");
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-
+        context = getApplicationContext();
+        activity = MainActivity.this;
+        AccountManager accountManager = AccountManager.get(this);
+        setContentView(R.layout.activity_main);
+        mProgressDialog = new ProgressDialog(activity);
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        mProgressDialog.setTitle("AsyncTask");
+        mProgressDialog.setMessage("Please wait, we are downloading your image file...");
+        URL logoURL = logoToUrl();
+        URL leftURL = leftSideImageToUrl();
+        mMyTask = new DownloadTask().execute(logoURL, leftURL);
+        imageViewLogo = findViewById(R.id.logoImage);
+        imageViewLeftSide = findViewById(R.id.leftSideImage);
 
     }
 
@@ -48,13 +69,10 @@ public class MainActivity extends AppCompatActivity {
     {
 
         TextView showNumbers = findViewById(R.id.textVinput);
-
-
         Toast tooFewNumbersToast = Toast.makeText(this, "Please Enter 10 Numbers", Toast.LENGTH_LONG);
         tooFewNumbersToast.setGravity(Gravity.TOP| Gravity.CENTER_HORIZONTAL, 0, 0);
 
         View tooFewview = tooFewNumbersToast.getView();
-        //view.setBackgroundResource(R.drawable.custom_background);
         TextView tooFewTextView = (TextView) tooFewview.findViewById(android.R.id.message);
         tooFewTextView.setTextColor(Color.parseColor("#fc030b"));
 
@@ -254,5 +272,65 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private class DownloadTask extends AsyncTask<URL,Void,Bitmap>{
+        protected void onPreExecute(){
+            mProgressDialog.show();
+        }
+        protected Bitmap doInBackground(URL... urls){
+            HttpURLConnection connection = null;
+            URL logoURL = urls[0];
+            URL leftURL = urls[1];
+            try{
+                connection = (HttpURLConnection) logoURL.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+                Bitmap logoBitmap = BitmapFactory.decodeStream(bufferedInputStream);
+                connection = (HttpURLConnection) leftURL.openConnection();
+                connection.connect();
+                InputStream lInputStream = connection.getInputStream();
+                BufferedInputStream lBufferedInputStream = new BufferedInputStream(lInputStream);
+                Bitmap leftBitmap = BitmapFactory.decodeStream(lBufferedInputStream);
+                List<Bitmap> bitmapList = new ArrayList<>();
+                bitmapList.add(logoBitmap);
+                bitmapList.add(leftBitmap);
+                return bitmapList;
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+        // When all async task done
+        protected void onPostExecute(Bitmap result){
+            // Hide the progress dialog
+            mProgressDialog.dismiss();
+            if(result!=null){
+                //resizedBM = getResizedBitmap(result, 40, 40);
+                resizedBM = result.createScaledBitmap(result, 150,150, true);
+                imageViewLogo.setImageBitmap(resizedBM);
+            } else {
+                // Notify user that an error occurred while downloading image
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    protected URL logoToUrl() {
+        try {
+            url = new URL("https://app-images-central.s3.us-east-2.amazonaws.com/logo/engaged_logo_green.png");
+            return url;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    protected URL leftSideImageToUrl() {
+        try {
+            url = new URL("https://app-images-central.s3.us-east-2.amazonaws.com/neighbors.jpg");
+            return url;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
