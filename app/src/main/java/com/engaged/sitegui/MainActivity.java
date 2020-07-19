b1a026fd0a95923866920028c958035d72e3805b
dadecker <daviddecker21@yahoo.com>
2020-07-19 16:28:34 -0400

    two image imports/cache management, styled - need api call and more styling

--- app/src/main/java/com/engaged/sitegui/MainActivity.java
+++ app/src/main/java/com/engaged/sitegui/MainActivity.java
@@ -3,64 +3,45 @@ package com.engaged.sitegui;
 
 import android.accounts.AccountManager;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.squareup.picasso.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private static String numberInput = "";
+    private String logoStr = "https://app-images-central.s3.us-east-2.amazonaws.com/logo1.png";
+    private String leftSideStr = "https://app-images-central.s3.us-east-2.amazonaws.com/friends1.jpg";
     Context context;
     Activity activity;
-    CoordinatorLayout coordinatorLayout;
-    Button downloadbutton;
-    ImageView imageViewLogo;
-    ImageView imageViewLeftSide;
-    ProgressDialog mProgressDialog;
     URL url;
-    AsyncTask mMyTask;
-    AsyncTask mTaskTwo;
-    Bitmap resizedBM;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = getApplicationContext();
         activity = MainActivity.this;
-        AccountManager accountManager = AccountManager.get(this);
         setContentView(R.layout.activity_main);
-        mProgressDialog = new ProgressDialog(activity);
-        mProgressDialog.setIndeterminate(true);
-        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        mProgressDialog.setTitle("AsyncTask");
-        mProgressDialog.setMessage("Please wait, we are downloading your image file...");
-        URL logoURL = logoToUrl();
-        URL leftURL = leftSideImageToUrl();
-        mMyTask = new DownloadTask().execute(logoURL, leftURL);
-        imageViewLogo = findViewById(R.id.logoImage);
-        imageViewLeftSide = findViewById(R.id.leftSideImage);
+        ImageView imageViewLogo = findViewById(R.id.logoImage);
+        ImageView imageViewLeftSide = findViewById(R.id.leftSideImage);
+        Picasso.get().load(logoStr).resize(150,150).centerCrop().into(imageViewLogo);
+        Picasso.get().load(leftSideStr).resize(500,500).centerCrop().into(imageViewLeftSide);
 
     }
 
@@ -104,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                     TooManyNumbersToast.show();
                 }
                 break;
-                case R.id.button1:
+            case R.id.button1:
                     if (numberInput.length() < 16)
                     {
                         if (numberInput.length() == 3 || numberInput.length() == 9)
@@ -272,65 +253,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class DownloadTask extends AsyncTask<URL,Void,Bitmap>{
-        protected void onPreExecute(){
-            mProgressDialog.show();
-        }
-        protected Bitmap doInBackground(URL... urls){
-            HttpURLConnection connection = null;
-            URL logoURL = urls[0];
-            URL leftURL = urls[1];
-            try{
-                connection = (HttpURLConnection) logoURL.openConnection();
-                connection.connect();
-                InputStream inputStream = connection.getInputStream();
-                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-                Bitmap logoBitmap = BitmapFactory.decodeStream(bufferedInputStream);
-                connection = (HttpURLConnection) leftURL.openConnection();
-                connection.connect();
-                InputStream lInputStream = connection.getInputStream();
-                BufferedInputStream lBufferedInputStream = new BufferedInputStream(lInputStream);
-                Bitmap leftBitmap = BitmapFactory.decodeStream(lBufferedInputStream);
-                List<Bitmap> bitmapList = new ArrayList<>();
-                bitmapList.add(logoBitmap);
-                bitmapList.add(leftBitmap);
-                return bitmapList;
-            }catch(IOException e){
-                e.printStackTrace();
-            }
-            return null;
-        }
-        // When all async task done
-        protected void onPostExecute(Bitmap result){
-            // Hide the progress dialog
-            mProgressDialog.dismiss();
-            if(result!=null){
-                //resizedBM = getResizedBitmap(result, 40, 40);
-                resizedBM = result.createScaledBitmap(result, 150,150, true);
-                imageViewLogo.setImageBitmap(resizedBM);
-            } else {
-                // Notify user that an error occurred while downloading image
-                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-    protected URL logoToUrl() {
-        try {
-            url = new URL("https://app-images-central.s3.us-east-2.amazonaws.com/logo/engaged_logo_green.png");
-            return url;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    protected URL leftSideImageToUrl() {
-        try {
-            url = new URL("https://app-images-central.s3.us-east-2.amazonaws.com/neighbors.jpg");
-            return url;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 }
